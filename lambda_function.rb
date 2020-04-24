31c146cc2ad07b2b8fb1bd3cc2c21f1bfcbe64ac
tsunagaru-git <tsunagaru.git@gmail.com>
2020-04-24 13:12:51 +0000

    first commit

---
+++ lambda_function.rb
@@ -0,0 +1,25 @@
+require 'json'
+require 'httpclient'
+require 'jwt'
+
+require 'rubygems'
+require 'active_record'
+
+def lambda_handler(event:, context:)
+  
+  base_url = 'https://www.yahoo.co.jp/'
+  client = HTTPClient.new()
+  response = client.get(base_url)
+
+  
+  r = {
+    version: "gem check1  ruby version= #{RUBY_VERSION}",
+#    rails_version: "ActiveRecord.class is #{ActiveRecord}",
+    endless: [0, 1, 2][0..],
+    active_record_class: ActiveRecord.class,
+    body: response.body,
+    status: response.status
+  }
+  { statusCode: 200, body: JSON.generate(r) }
+end
+
